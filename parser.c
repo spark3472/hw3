48e55211877717769fd990485f40b2edce2e1b24
Ashley Park <apark2@brynmawr.edu>
2021-09-27 20:08:08 -0400

    helpful message

--- parser.c
+++ parser.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
@@ -6,11 +5,9 @@
 #include <string.h>
 #include <readline/readline.h>
 #include <readline/history.h>
-=======
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
->>>>>>> b814c4d22d3428ebcde7da8bc1eaa94a0fcc1faf
 
 #define TRUE  1;
 #define FALSE 0;
@@ -23,13 +20,14 @@ typedef struct tokenizer{
 } TOKENIZER;
 //global array toks
 char** toks;
+
 /* Gets the next delimiter or the string between delimiters
  * @param tokenizer
  * @return Pointer to the string between the delimiters or the delimiter
  */
 
 int a = 0;
-char* get_next_token(TOKENIZER *t){
+char* get_next_token(TOKENIZER *v){
 	//if current char is a delimiter, just return it
 //else go until next char is a delimiter
 //return the substring without white spaces
@@ -38,31 +36,29 @@ char* get_next_token(TOKENIZER *t){
   
   char* string;
   int b = 0;
-  if(*(t->pos) == '\0'){
+  if(*(v->pos) == '\0'){
     return NULL;
   }
-  if (*(t->pos) == '&'||*(t->pos)==';'){
+  if (*(v->pos) == '&'||*(v->pos)==';'){
     b++;
-    t->pos++;
+    v->pos++;
     string = (char*) malloc((b+1)*sizeof(char));
-    memcpy(string, &t->str[a], b);
+    memcpy(string, &v->str[a], b);
     a += b;
-    printf("%s\n", string);
     return string;
   }
 
-  while(*(t->pos) != '\0'){
-    if (*(t->pos) == '&'||*(t->pos) == ';'){
+  while(*(v->pos) != '\0'){
+    if (*(v->pos) == '&'||*(v->pos) == ';'){
       break;
     }else{
-      t->pos++;
+      v->pos++;
       b++;
     }
   }
   string = (char*) malloc((b+1)*sizeof(char));
-  memcpy(string, &t->str[a], b);
+  memcpy(string, &v->str[a], b);
   a += b;
-  printf("%s\n", string);
   return string;
 }
 
@@ -71,20 +67,21 @@ char* get_next_token(TOKENIZER *t){
  * @return pointer that points to the start of the line
  */
 TOKENIZER init_tokenizer(char* line){
-  TOKENIZER t;
-  t.str = line;
-  t.pos = t.str;
-  return t;
+  TOKENIZER s;
+  s.str = line;
+  s.pos = s.str;
+  return s;
 }
 
 /* Stores pointers to tokens in global array toks
  * @return Number of tokens
  */
 TOKENIZER t;
+TOKENIZER u;
 char* line;
 char* shell_prompt = "parser> ";
 int parser(){
-  int i = 0;int n = 0;a = 0;
+  int n = 0;int i = 0;
   line = readline(shell_prompt);
   //ctrl-d
   if(line == NULL){
@@ -96,21 +93,30 @@ int parser(){
   }
   t = init_tokenizer(line);
   //how many tokens
-  while(get_next_token(&t) != NULL){
+  char* string;
+  while((string = get_next_token(&t)) != NULL){
     n++;
   }
   //allocate pointers to tokens +1 for the ending NULL
-  /*toks = (char*) malloc(sizeof(char*)*(n+1));
-	//start from beginning again
-	t = init_tokenizer(line);
-	while ((toks[i++] = get_next_token(t)) != NULL);
-	free(line);*/
+  toks = (char**) malloc(sizeof(char*)*(n+1));
+  //start from beginning again
+  u = init_tokenizer(line);
+  a = 0;
+  for (int i = 0; i < n; i++){
+    char* string = get_next_token(&u);
+    toks[i] = (char*)malloc(strlen(string)*sizeof(char));
+    strcpy(toks[i], string);
+  }
+  free(line);
   return n;
 }
 
 int main(){
+  int number;
   while(1){
-    int number = parser();
-    printf("%d\n", number);
+    number = parser();
+    for (int i = 0; i < number; i++){
+      printf("%s\n", toks[i]);
+    }
   }
 }
